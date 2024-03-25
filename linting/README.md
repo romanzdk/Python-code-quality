3739f90d7fedb41bf48345fb6a70e96f5212ecfd
Roman Zydyk <romanzdk@gmail.com>
2024-03-25 18:37:58 +0100

    Add linting resources

---
+++ linting/README.md
@@ -0,0 +1,3 @@
+1. Install dependencies
+
+- `poetry add pylint ruff`
---
+++ linting/linting.py
@@ -0,0 +1,46 @@
+# # Missing colon
+# for i in range(5)
+#     print(i)
+
+###############################################################################
+
+# Naming style
+# MyVariable = 10
+
+# ##############################################################################
+
+# # # Unused import
+# import os
+
+# SOME_CONSTANT = 42
+
+##############################################################################
+
+# # Indentation error
+
+# def my_function():
+#     print("Hello")
+#      print("World")
+
+# ##############################################################################
+
+# # Singleton comparison
+# def check_none(value):
+#     if value == None:
+#         return True
+#     return False
+
+# ##############################################################################
+
+# # Use of eval
+# user_input = input("Enter your command: ")
+# eval(user_input)
+
+# ##############################################################################
+
+# # Not using context manager
+# file = open("data.txt", "r")
+# data = file.read()
+# file.close()
+
+# ##############################################################################
